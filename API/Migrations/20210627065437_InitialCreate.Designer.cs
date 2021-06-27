7ac8d2060a9e419d343c0c57b6d976a3115e45ed
Shafran Ashraff <shafran_@outlook.com>
2021-06-27 12:26:09 +0530

    Added migrations

--- API/Migrations/20210627065437_InitialCreate.Designer.cs
+++ API/Migrations/20210627065437_InitialCreate.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20210626142306_InitialCreate")]
+    [Migration("20210627065437_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
